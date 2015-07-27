2c1d475988e3125b58f909f21a89d409ce349144
Xudong Liu <xudong.camsys@gmail.com>
2015-07-27 18:12:01 -0400

    tweak database_cleaner config

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,3 +1,7 @@
+require 'capybara/poltergeist'
+
+Capybara.asset_host = 'http://localhost:3000'
+
 Capybara.javascript_driver = :poltergeist
 
 Capybara.register_driver :poltergeist do |app|
--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -51,26 +51,15 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each, type: :feature) do
-    # :rack_test driver's Rack app under test shares database connection
-    # with the specs, so we can use transaction strategy for speed.
-    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
-
-    if driver_shares_db_connection_with_specs
-      DatabaseCleaner.strategy = :transaction
-    else
-      # Non-:rack_test driver is probably a driver for a JavaScript browser
-      # with a Rack app under test that does *not* share a database 
-      # connection with the specs, so we must use truncation strategy.
-      DatabaseCleaner.strategy = :truncation, {:except => %w[translations locales translation_keys]}
-    end
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation, {:except => %w[translations locales translation_keys]}
   end
 
   config.before(:each) do
     DatabaseCleaner.start
   end
 
-  config.after(:each) do
+  config.append_after(:each) do
     DatabaseCleaner.clean
   end
 
